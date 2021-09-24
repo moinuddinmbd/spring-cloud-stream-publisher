4d8fb459573bd0148087d961da637c6a37214536
Mohinuddin S <mohinuddin.salahuddin@hm.com>
2021-09-24 10:17:51 +0200

    Added support to insert partition key

--- src/main/java/com/moin/spring/cloud/stream/publisher/SpringKafkaPublisher.java
+++ src/main/java/com/moin/spring/cloud/stream/publisher/SpringKafkaPublisher.java
@@ -6,10 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.integration.support.MessageBuilder;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @EnableBinding(Source.class)
@@ -21,8 +25,8 @@ public class SpringKafkaPublisher {
 
     @PostMapping("/publish")
     @ApiOperation(value = "publishMessage", httpMethod = "POST", response = String.class, notes = "This service will publish messages to Kafka topic.")
-    public String publishMessage(@RequestBody String data) {
-        output.send(MessageBuilder.withPayload(data).build());
+    public String publishMessage(@RequestHeader("kafka_messageKey") String messageKey, @RequestBody String data) {
+        output.send(MessageBuilder.withPayload(data).setHeaderIfAbsent(KafkaHeaders.MESSAGE_KEY, messageKey.getBytes(StandardCharsets.UTF_8)).build());
         return "message published successfully";
     }
 }
